0763dcefe3f70644dbd51e2013b638ff2ad27976
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2023-10-03 18:01:11 -0400

    Start on simpleneuralnetwork implementation

---
+++ simpleneuralnetwork.py
@@ -0,0 +1,36 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+n_x, n_h, n_y = (2, 4, 1)
+parameters = initialize_parameters(n_x, n_h, n_y)
+print('parameters',parameters)
